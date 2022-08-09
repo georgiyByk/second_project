82c23d3222a3026d7f6ed6f7152d81d46429bf7d
Georgiy <gorra2009@yandex.ru>
2022-08-09 21:33:43 +0300

    add calc.py

---
+++ calc.py
@@ -0,0 +1,16 @@
+
+def step(number):
+
+	return number**2
+
+print(step(2))
+
+def max_num(number_1,number_2):
+
+	if number_1 > number_2:
+		return number_1
+	else:
+		return number_2
+
+print(max_num(2,5))
+
